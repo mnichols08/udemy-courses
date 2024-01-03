3faef9766f81e384f6dc2162ee0ec4b7ca963311
Mikey Nichols <mnix@journeytocode.io>
2024-01-02 19:28:48 -0500

    creates footer component

---
+++ free-code-camp/front-end-libraries/projects/25plus5/src/components/footer/footer.component.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <a href="https://codepen.io/mnichols08/" target="blank">
+          <i className="fas fa-code text-green"></i> with{" "}
+          <i className="fas fa-heart text-red"></i> by{" "}
+          <i className="fab fa-codepen text-yellow">mnichols08</i>
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
